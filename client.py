62788c14592dab1d51bf5fcfa2038e6009849a21
BMeach <43155420+BryceTech122@users.noreply.github.com>
2021-02-22 09:37:19 -0700

    1st working commit - tested

--- client.py
+++ client.py
@@ -12,20 +12,32 @@ with open('clientConfig.yml') as f:
     passwd = data.get('password')
     budir = data.get('backup_folder')
 
-# Connect to ftp
+# Initial connect to FTP to confirm credentials
 ftp = FTP(host=servip,user=uname,passwd=passwd)
-ftp.cwd(budir)
+ftp.close()
 
-# Copy files to local directory
+def current_time():
+    now = datetime.now()
+    time = now.strftime('%Y:%m:%d_%H:%M:%S')
+    return time
+
+# Function to copy files from budir to ./backup
 def copy_backup():
+    print('['+str(current_time())+'] Starting Download of All Backups')
+    ftp = FTP(host=servip,user=uname,passwd=passwd)
+    ftp.cwd(budir)
     files = ftp.nlst()
+    # TODO: If statment to autogenerate ./backup directory
     for file in files:
-        print("Downloading "+file)
-        ftp.retrbinary("RETR " + file ,open("./backups" + file, 'wb').write)
+        print('    - Downloading... '+file)
+        if file.endswith('.tar'):
+            ftp.retrbinary('RETR ' + file ,open('./backup/' + file, 'wb').write)
+    print('['+str(current_time())+'] Done Downloading All Backups\n')
     ftp.close()
 
-# Schedule backup
-schedule.every().minute.at(":30").do(copy_backup)
+# Schedule backup and run at client start
+schedule.every(5).minutes.at(":30").do(copy_backup)
+schedule.run_all()
 
 # Program loop
 while True:
--- server.py
+++ server.py
@@ -3,21 +3,47 @@ from datetime import datetime
 import schedule
 import time
 import yaml
+import os.path
+import os
+import tarfile
 
 # Open config file
 with open('serverConfig.yml') as f:  
     data = yaml.load(f, Loader=yaml.FullLoader)
-    mc_folder = data.get('mc_folder')
+    source_dir = data.get('mc_folder')
 
-# Zip Minecraft folder
-def make_backup(dir):
+# Function to get current time for logging
+def current_time():
+    now = datetime.now()
+    time = now.strftime('%Y:%m:%d_%H:%M:%S')
+    return time
+
+# Function to generate filename with .tar extenssion
+def make_filename():
     now = datetime.now()
     time = now.strftime("%Y:%m:%d_%H:%M:%S")
-    fname = "serverBackups/backup "+str(time)
-    shutil.make_archive(fname,'zip',dir)
+    fname = "backups/backup_"+str(time)+'.tar'
+    return fname
+
+# Function to remove old backups
+def remove_old(folder_path):
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.tar'):
+            os.remove(folder_path + file_name)
+
+# Function to tarfile your source_dir
+def make_tarfile(source_dir):
+    print('['+current_time()+'] Starting Backup')
+    remove_old('./backups/')
+    print('['+current_time()+'] Removing Previous Backups')
+    output_filename = make_filename()
+    with tarfile.open(output_filename, "w:gz") as tar:
+        print('['+current_time()+'] Starting Backup '+output_filename)
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+    print('['+current_time()+'] Backup Complete! '+output_filename)
 
 # Schedule zip
-schedule.every().minute.at(":00").do(make_backup, dir=mc_folder)
+schedule.every(5).minutes.at(":00").do(make_tarfile, source_dir=source_dir)
 
 # Program loop
 while True:
